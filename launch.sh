ac7870b7998fe32c706f74e3971a31a9e967135a
Aviv Shabi <62796670+avivshabi@users.noreply.github.com>
2022-04-18 10:18:52 +0300

    feat: added bash scripts for easy deployment to AWS

---
+++ launch.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+echo "Starting setup..."
+sudo apt -f install
+sudo apt -y update && sudo apt -y dist-upgrade
+sudo apt install git
+sudo apt -y install python3-pip
+echo "Installing AWS CLI..."
+pip3 install --upgrade awscli
+sudo apt install awscli zip
+echo "AWS CLI installed, please enter your credentials"
+aws configure
+sudo apt -y install build-essential libssl-dev libffi-dev python3-dev
+sudo apt install -y python3-venv
+git clone https://github.com/avivshabi/parkingLot.git app
+echo "Successfully cloned github code..."
+cd app
+echo "Installing required packages..."
+pip3 install -r requirements.txt
+echo "Creating dynamodb table..."
+aws dynamodb create-table --cli-input-json file://create-table.json
+echo "Stating FastAPI server..."
+uvicorn main:app --reload >/dev/null & exit
